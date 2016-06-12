660590d35d4cef2a5fd3bc0a388249ba762fa070
Maxime Wojtczak <max@imac-de-maxime.home>
2016-06-12 17:45:06 +0200

    feat(Rendering) : Choice of grid and font.

--- dist/scripts/grid.js
+++ dist/scripts/grid.js
@@ -1,6 +1,26 @@
 function render() {
+
+    rendering.className = '';
+
+    // Setting text
     var text = document.getElementById('text').value;
-    text = text.replace(/\n/g, '<br/>');
+    text = text.replace(/\n/g, '<br/><br/>');
     text = text.replace(/\r/g, '');
     document.getElementById('rendering').innerHTML = text;
+
+    // Setting font
+    setFont(document.getElementById('font').value);
+
+    // Setting font
+    setGrid(document.getElementById('grid').value);
+}
+
+function setFont(font) {
+    var rendering = document.getElementById('rendering');
+    rendering.classList.add('font-'+ font);
+}
+
+function setGrid(grid) {
+    var rendering = document.getElementById('rendering');
+    rendering.classList.add('grid-'+ grid);
 }
